f589059879c91e05550123c71092008fbc0bf198
sofya.egorycheva <sofya.egorycheva@simbirsoft.com>
2023-05-14 23:45:04 +0300

    final commit

--- src/test/java/quru/com/potter/pages/FormPage.java
+++ src/test/java/quru/com/potter/pages/FormPage.java
@@ -2,7 +2,9 @@ package quru.com.potter.pages;
 
 import com.codeborne.selenide.SelenideElement;
 import quru.com.potter.pages.components.Calendar;
+import quru.com.potter.test.TestData;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,12 +14,21 @@ public class FormPage {
             lastName = $("#lastName"),
             userEmail = $("#userEmail"),
             userAddress = $("#currentAddress"),
-            userNumber = $("#userNumber");
+            userNumber = $("#userNumber"),
+            femaleRadio = $(".custom-control-label[for='gender-radio-2']"),
+            subjectInput = $("#subjectsInput"),
+            musicCheckbox = $(".custom-control-label[for='hobbies-checkbox-3']"),
+            uploadPicture = $("#uploadPicture"),
+            stateField = $("#react-select-3-input"),
+            cityField = $("#react-select-4-input"),
+            modalTitle = $("#example-modal-sizes-title-lg"),
+            responseTable = $(".table-responsive");
+    private final String URL = "/automation-practice-form";
 
     private Calendar calendar = new Calendar();
 
     public FormPage openPage() {
-        open("https://demoqa.com/automation-practice-form");
+        open(URL);
         return this;
     }
 
@@ -46,9 +57,43 @@ public class FormPage {
         return this;
     }
 
-    public FormPage fillUserNumber(String number){
+    public FormPage fillUserNumber(String number) {
         userNumber.setValue(number);
         return this;
     }
 
+    public FormPage femaleSelect() {
+        femaleRadio.click();
+        return this;
+    }
+
+    public FormPage fillSubject(String value) {
+        subjectInput.setValue(value).pressEnter();
+        return this;
+    }
+
+    public FormPage selectMusicCheckbox() {
+        musicCheckbox.click();
+        return this;
+    }
+
+    public FormPage uploadPicture(String fileName) {
+        uploadPicture.uploadFromClasspath(fileName);
+        return this;
+    }
+
+    public FormPage fillState(String value) {
+        stateField.setValue(value).pressEnter();
+        return this;
+    }
+
+    public FormPage fillCity(String value) {
+        cityField.setValue(value).pressEnter().pressEnter();
+        return this;
+    }
+
+    public void checkForm() {
+        modalTitle.shouldHave(text("Thanks for submitting the form"));
+        responseTable.shouldHave(text(TestData.firstName + " " + TestData.lastName));
+    }
 }
--- src/test/java/quru/com/potter/test/FormTest.java
+++ src/test/java/quru/com/potter/test/FormTest.java
@@ -13,33 +13,23 @@ public class FormTest {
     private FormPage formPage = new FormPage();
     Faker faker = new Faker();
 
-    @BeforeAll
-    static void beforeAll() {
-
-        Configuration.pageLoadTimeout = 60000;
-
-    }
-
     @Test
     void fillFormTest() {
+        Configuration.pageLoadTimeout=60000;
+        Configuration.baseUrl = "https://demoqa.com";
         formPage.openPage()
                 .fillName(TestData.firstName)
                 .fillLastName(TestData.lastName)
                 .fillCalendar(TestData.day, TestData.month, TestData.year)
                 .fillUserEmail(faker.internet().emailAddress())
                 .fillUserAddress(TestData.currentAddress)
-                .fillUserNumber(faker.phoneNumber().subscriberNumber(10));
-        $(".custom-control-label[for='gender-radio-2']").click();
-        $("#subjectsInput").setValue("Computer Science").pressEnter();
-        $(".custom-control-label[for='hobbies-checkbox-3']").click();
-        $("#uploadPicture").uploadFromClasspath("photo.webp");
-        $("#react-select-3-input").setValue("NCR").pressEnter();
-        $("#react-select-4-input").setValue("Delhi").pressEnter().pressEnter();
-
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text(TestData.firstName + " " + TestData.lastName));
+                .fillUserNumber(faker.phoneNumber().subscriberNumber(10))
+                .femaleSelect()
+                .fillSubject("Computer Science")
+                .selectMusicCheckbox()
+                .uploadPicture("photo.webp")
+                .fillState("NCR")
+                .fillCity("Delhi")
+                .checkForm();
     }
-
 }
-
-
