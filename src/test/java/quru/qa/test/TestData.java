90a0746dc96dda7d7533a46eaceaf990ac7e922b
sofya.egorycheva <sofya.egorycheva@simbirsoft.com>
2023-05-14 22:53:36 +0300

    completed

---
+++ src/test/java/quru/qa/test/TestData.java
@@ -0,0 +1,13 @@
+package quru.qa.test;
+
+public class TestData {
+    public static String
+            firstName = "Sophie",
+            lastName = "Potter",
+            day ="14",
+            month ="11",
+            year ="1997",
+            currentAddress = "address 1";
+
+}
+
