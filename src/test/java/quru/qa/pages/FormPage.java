e3f1b2b7b73721f6f049996ae4fbc5fd6d4154fc
sofya.egorycheva <sofya.egorycheva@simbirsoft.com>
2023-05-14 22:55:14 +0300

    added forgotten classes

---
+++ src/test/java/quru/qa/pages/FormPage.java
@@ -0,0 +1,54 @@
+package quru.qa.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import quru.qa.pages.components.Calendar;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FormPage {
+    private SelenideElement
+            firstName = $("#firstName"),
+            lastName = $("#lastName"),
+            userEmail = $("#userEmail"),
+            userAddress = $("#currentAddress"),
+            userNumber = $("#userNumber");
+
+    private Calendar calendar = new Calendar();
+
+    public FormPage openPage() {
+        open("https://demoqa.com/automation-practice-form");
+        return this;
+    }
+
+    public FormPage fillName(String value) {
+        firstName.setValue(value);
+        return this;
+    }
+
+    public FormPage fillLastName(String value) {
+        lastName.setValue(value);
+        return this;
+    }
+
+    public FormPage fillCalendar(String day, String month, String year) {
+        calendar.setDate(day, month, year);
+        return this;
+    }
+
+    public FormPage fillUserEmail(String email) {
+        userEmail.setValue(email);
+        return this;
+    }
+
+    public FormPage fillUserAddress(String address) {
+        userAddress.setValue(address);
+        return this;
+    }
+
+    public FormPage fillUserNumber(String number){
+        userNumber.setValue(number);
+        return this;
+    }
+
+}
---
+++ src/test/java/quru/qa/pages/components/Calendar.java
@@ -0,0 +1,13 @@
+package quru.qa.pages.components;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class Calendar {
+    public void setDate(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue(month);
+        $(".react-datepicker__year-select").selectOptionByValue(year);
+        $(".react-datepicker__day.react-datepicker__day--0"+ day +".react-datepicker__day--weekend").click();
+
+    }
+}
