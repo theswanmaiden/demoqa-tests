8e2cb48559f5e2f82178039c1335649403a4c7ee
sofya.egorycheva <sofya.egorycheva@simbirsoft.com>
2023-06-19 23:50:47 +0300

    add Jenkins

--- src/test/java/com/potter/test/FormTest.java
+++ src/test/java/com/potter/test/FormTest.java
@@ -1,9 +1,14 @@
 package com.potter.test;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
+import com.potter.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import com.potter.pages.FormPage;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -13,8 +18,18 @@ public class FormTest {
 
     @Test
     void fillFormTest() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
         Configuration.pageLoadTimeout=60000;
         Configuration.baseUrl = "https://demoqa.com";
+
         formPage.openPage()
                 .fillName(TestData.firstName)
                 .fillLastName(TestData.lastName)
@@ -29,5 +44,11 @@ public class FormTest {
                 .fillState("NCR")
                 .fillCity("Delhi")
                 .checkForm();
+
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
+
 }
