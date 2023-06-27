d2175435091a6aa4a37b7b24a52eed89df3508d1
sofya.egorycheva <sofya.egorycheva@simbirsoft.com>
2023-06-28 00:39:30 +0300

    added forgotten annotation

--- src/test/java/com/potter/config/CredentialsConfig.java
+++ src/test/java/com/potter/config/CredentialsConfig.java
@@ -2,6 +2,7 @@ package com.potter.config;
 
 import org.aeonbits.owner.Config;
 
+@Config.Sources({"classpath:config/credentials.properties"})
 public interface CredentialsConfig extends Config {
     String login();
     String password();
--- src/test/java/com/potter/properties/OwnerTests.java
+++ src/test/java/com/potter/properties/OwnerTests.java
@@ -2,8 +2,12 @@ package com.potter.properties;
 
 import com.potter.config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
+@Tag("owner")
 public class OwnerTests {
     public CredentialsConfig credentials =
             ConfigFactory.create(CredentialsConfig.class);
@@ -13,6 +17,7 @@ public class OwnerTests {
         String login = credentials.login();
         String password = credentials.password();
 
-        System.out.println(login + " - это логин, а это" + " " + password + " - пароль");
+        String message = format("Это логин %s , а это пароль %s", login, password);
+        System.out.println(message);
     }
 }
